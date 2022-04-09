871224c666ff8771138cba56201e2a383afab5bc
Maya Baruch <maya.baruch.40@gmail.com>
2022-04-10 00:08:15 +0300

    detect balloon color works better

--- balloon.py
+++ balloon.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 
-
 THRESHOLD_SIZE = 15
 BALLOON_H_RANGE = 20
 BALLOON_S_RANGE = 20
@@ -14,13 +13,11 @@ GREEN_LOWER_BOUNDS = (25, 52, 72)
 GREEN_UPPER_BOUNDS = (102, 255, 255)
 BLUE_LOWER_BOUNDS = (94, 80, 2)
 BLUE_UPPER_BOUNDS = (120, 255, 255)
-PINK_LOWER_BOUNDS = (136, 87, 111)
-PINK_UPPER_BOUNDS = (180, 255, 255)
 NO_LOWER_BOUNDS = (0, 0, 0)
 NO_UPPER_BOUNDS = (255,255, 255)
 
 
-def detect_color(frame):
+def detect_balloon_color(frame):
     y_shape = frame.shape[0]
     x_shape = frame.shape[1]
     crop_img = frame[int(y_shape/4) : int(3*y_shape/4) , int(x_shape/4) : int(3*x_shape/4)]
@@ -29,12 +26,18 @@ def detect_color(frame):
     s = hsv[:,:,1]
     v = hsv[:,:,2]
     ball_color = (int(np.median(h)), int(np.median(s)), int(np.median(v)))
-    min_color = (max(0,int(np.min(h))-BALLOON_H_RANGE), 
-                    max(0,int(np.min(s))-BALLOON_S_RANGE), 
-                    max(0,int(np.min(v))-BALLOON_V_RANGE))
-    max_color = (min(255,int(np.max(h))+BALLOON_H_RANGE), 
-                    min(255,int(np.max(s))+BALLOON_S_RANGE), 
-                    min(255,int(np.max(v))+BALLOON_V_RANGE))
+    # min_color = (max(0,int(np.min(h))-BALLOON_H_RANGE), 
+    #                 max(0,int(np.min(s))-BALLOON_S_RANGE), 
+    #                 max(0,int(np.min(v))-BALLOON_V_RANGE))
+    # max_color = (min(255,int(np.max(h))+BALLOON_H_RANGE), 
+    #                 min(255,int(np.max(s))+BALLOON_S_RANGE), 
+    #                 min(255,int(np.max(v))+BALLOON_V_RANGE))
+    min_color = (max(0,ball_color[0]-BALLOON_H_RANGE), 
+                    max(0,ball_color[1]-BALLOON_S_RANGE), 
+                    max(0,ball_color[2]-BALLOON_V_RANGE))
+    max_color = (min(255,ball_color[0]+BALLOON_H_RANGE), 
+                    min(255,ball_color[1]+BALLOON_S_RANGE), 
+                    min(255,ball_color[2]+BALLOON_V_RANGE))
     return min_color,max_color
 
 
@@ -63,7 +66,7 @@ def capture_video():
         key = cv2.waitKey(1)
         # the 'b' button is set as the detect color of balloon
         if key & 0xFF == ord('b'):
-            balloon_lower_bound, balloon_upper_bound = detect_color(frame)
+            balloon_lower_bound, balloon_upper_bound = detect_balloon_color(frame)
             print(2,balloon_lower_bound, balloon_upper_bound)
 
         # the 'q' button is set as the quitting button
