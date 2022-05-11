fixes of distance calculation + rename web, phone to left, right

detection_2d.py
@@ -7,17 +7,25 @@ NO_LOWER_BOUNDS = (0, 0, 0)
 NO_UPPER_BOUNDS = (255,255, 255)
 
 
+class Camera:
+
+    def __init__(self, fov, index, is_flipped=False):
+        self.fov = np.radians(fov)
+        self.index = index
+        self.flip = -1 if is_flipped else 1
+
+
 def interactive_loop(key, image_3d, colors):
     continue_loop = True
-    # the 'w' button is set as the detect color of balloon in the web cam
-    if key == ord('w'):
-        lower, upper = image_3d.frame_web.detect_color()
-        colors.ball_web.change(lower, upper)
+    # the 'l' button is set as the detect color of balloon in the left cam
+    if key == ord('l'):
+        lower, upper = image_3d.frame_left.detect_color()
+        colors.ball_left.change(lower, upper)
 
-    # the 'p' button is set as the detect color of balloon in the phone cam
-    elif key == ord('p'):
-        lower, upper = image_3d.frame_phone.detect_color()
-        colors.ball_phone.change(lower, upper)
+    # the 'r' button is set as the detect color of balloon in the right cam
+    elif key == ord('r'):
+        lower, upper = image_3d.frame_right.detect_color()
+        colors.ball_right.change(lower, upper)
 
     # the 'q' button is set as the quitting button
     elif key == ord('q'):
@@ -26,67 +34,69 @@ def interactive_loop(key, image_3d, colors):
     return continue_loop
 
 
-def capture_video(cameras_distance, web_fov, phone_fov):
-    vid_web = cv2.VideoCapture(0)
-    vid_phone = cv2.VideoCapture(1)
+def capture_video(cameras_distance, left, right):
+    vid_left = cv2.VideoCapture(left.index)
+    vid_right = cv2.VideoCapture(right.index)
 
-    detect_web_time = 150
-    detect_phone_time = 300
+    detect_left_time = 150
+    detect_right_time = 300
 
     frame_counter = 0
 
     image_old = None
 
     colors = ColorBounds()
-  
-    while(True):  
+    while(True):
         frame_counter = frame_counter+1
         # Capture the video frame by frame
-        ret_web, image_web = vid_web.read()
-        ret_phone, image_phone = vid_phone.read()
+        ret_left, image_left = vid_left.read()
+        ret_right, image_right = vid_right.read()
 
-        image_web = cv2.flip(image_web, 1)
-        image_now = Image3D(image_web, image_phone)
+        image_left = cv2.flip(image_left, 1)
+        image_now = Image3D(image_left, image_right)
         text_balloon = None
     
         # Process frames
         if frame_counter>1:
-            image_now.frame_web.detect_balloon(colors.ball_web, image_old.frame_web.x_balloon, image_old.frame_web.y_balloon)
-            image_now.frame_phone.detect_balloon(colors.ball_phone, image_old.frame_phone.x_balloon, image_old.frame_phone.y_balloon)
-            if image_now.frame_web.x_balloon!=0 and image_now.frame_phone.x_balloon!=0:
-                image_now.calculate_distance(web_fov, phone_fov, cameras_distance)
+            image_now.frame_left.detect_balloon(colors.ball_left, image_old.frame_left.x_balloon, image_old.frame_left.y_balloon)
+            image_now.frame_right.detect_balloon(colors.ball_right, image_old.frame_right.x_balloon, image_old.frame_right.y_balloon)
+            if image_now.frame_left.x_balloon!=0 and image_now.frame_right.x_balloon!=0:
+                image_now.calculate_distance(left, right, cameras_distance)
                 text_balloon = "(%.0f, %.0f)" % (image_now.phys_x, image_now.phys_y)
 
         # Display the resulting frame
-        image_now.frame_web.show_image("web")
-        image_now.frame_phone.show_image("phone", text_balloon=text_balloon)
+        image_now.frame_left.show_image("left")
+        image_now.frame_right.show_image("right", text_balloon=text_balloon)
 
         image_old = image_now
 
         key = cv2.waitKey(1) & 0xFF
-        if frame_counter == detect_web_time:
-            print("detecting ballon color web")
+        if frame_counter == detect_left_time:
+            print("detecting ballon color left")
             key = ord('w')
-        if frame_counter == detect_phone_time:
-            print("detecting ballon color phone")
+        if frame_counter == detect_right_time:
+            print("detecting ballon color right")
             key = ord('p')    
         continue_loop = interactive_loop(key, image_now, colors)
         if not continue_loop:
             break
   
     # After the loop release the cap object
-    vid_web.release()
-    vid_phone.release()
+    vid_left.release()
+    vid_right.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
 
 
 # return how much cm in one pixel.
 def pixels_to_cm(distance, num_pixels, fov_angle):  
-    return distance * 2 * np.tan(fov_angle/2) / num_pixels
+    return distance * 2 * np.tan(fov_angle/2) * fov_angle / num_pixels
 
 
 if __name__ == "__main__":
+    web = Camera(51.3, 0, True)
+    phone = Camera(66.9, 2, False)
+    distance = 46.5
     # Galaxy - FoV is 67 degrees
     # Lenovo - FoV is 61 degrees
-    capture_video(119, np.radians(61), np.radians(67))
+    capture_video(distance, web, phone)

frame.py
@@ -1,15 +1,14 @@
 import numpy as np
 import cv2
-
+import time
 
 class Frame:
-
-    THRESHOLD_SIZE = 8 # pixels
+    THRESHOLD_SIZE = 8  # pixels
     H_RANGE = 20
     S_RANGE = 30
     V_RANGE = 150
 
-    SEARCH_RANGE = 70 # pixels
+    SEARCH_RANGE = 70  # pixels
 
     def __init__(self, image):
         self.image = image
@@ -20,20 +19,19 @@ class Frame:
 
     def detect_coordinates(self, bounds, x_old, y_old):
         x_min, x_max, y_min, y_max = 0, self.image.shape[1], 0, self.image.shape[0]
-        if x_old!=0 and y_old!=0:
+        if x_old != 0 and y_old != 0:
             x_min = max(int(x_old - Frame.SEARCH_RANGE), x_min)
-            x_max = min(int(x_old + Frame.SEARCH_RANGE)+1, x_max)
+            x_max = min(int(x_old + Frame.SEARCH_RANGE) + 1, x_max)
             y_min = max(int(y_old - Frame.SEARCH_RANGE), y_min)
-            y_max = min(int(y_old + Frame.SEARCH_RANGE)+1, y_max)
+            y_max = min(int(y_old + Frame.SEARCH_RANGE) + 1, y_max)
 
         detection_image = self.image[y_min:y_max, x_min:x_max]
 
         # convert to hsv
         hsv = cv2.cvtColor(detection_image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, bounds.lower, bounds.upper)
-
-        #define kernel size  
-        kernel = np.ones((Frame.THRESHOLD_SIZE,Frame.THRESHOLD_SIZE), np.uint8)
+        # define kernel size
+        kernel = np.ones((Frame.THRESHOLD_SIZE, Frame.THRESHOLD_SIZE), np.uint8)
         # Remove unnecessary noise from mask
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -45,8 +43,8 @@ class Frame:
         balloon_pixels = np.argwhere(mask)
         if len(balloon_pixels) == 0:
             return 0, 0
-        x_coor = np.median(balloon_pixels[:,1])
-        y_coor = np.median(balloon_pixels[:,0])
+        x_coor = np.median(balloon_pixels[:, 1])
+        y_coor = np.median(balloon_pixels[:, 0])
 
         return x_coor + x_min, y_coor + y_min
 
@@ -63,34 +61,37 @@ class Frame:
     def detect_color(self):
         y_shape = self.image.shape[0]
         x_shape = self.image.shape[1]
-        crop_img = self.image[int(y_shape/3) : int(2*y_shape/3), int(x_shape/3) : int(2*x_shape/3)]
+        crop_img = self.image[int(y_shape / 3): int(2 * y_shape / 3), int(x_shape / 3): int(2 * x_shape / 3)]
         hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-        h = hsv[:,:,0]
-        s = hsv[:,:,1]
-        v = hsv[:,:,2]
+        h = hsv[:, :, 0]
+        s = hsv[:, :, 1]
+        v = hsv[:, :, 2]
         ball_color = (int(np.median(h)), int(np.median(s)), int(np.median(v)))
-        min_color = (max(0, ball_color[0] - Frame.H_RANGE), max(0, ball_color[1] - Frame.S_RANGE), max(20, ball_color[2] - Frame.V_RANGE))
-        max_color = (min(255, ball_color[0] + Frame.H_RANGE), min(255, ball_color[1] + Frame.S_RANGE), min(255, ball_color[2] + Frame.V_RANGE))
+        min_color = (max(0, ball_color[0] - Frame.H_RANGE), max(0, ball_color[1] - Frame.S_RANGE),
+                     max(20, ball_color[2] - Frame.V_RANGE))
+        max_color = (min(255, ball_color[0] + Frame.H_RANGE), min(255, ball_color[1] + Frame.S_RANGE),
+                     min(255, ball_color[2] + Frame.V_RANGE))
         return [min_color, max_color]
 
-    def show_image(self, window_name, detection_sign = True, text_balloon = None, text_drone = None):
+    def show_image(self, window_name, detection_sign=True, text_balloon=None, text_drone=None):
         show_img = self.image
-        if detection_sign and self.x_balloon!=0 and self.y_balloon!=0:
-            show_img = cv2.circle(show_img, (int(self.x_balloon), int(self.y_balloon)), 15, (0,0,0), 3)
+        if detection_sign and self.x_balloon != 0 and self.y_balloon != 0:
+            show_img = cv2.circle(show_img, (int(self.x_balloon), int(self.y_balloon)), 15, (0, 0, 0), 3)
             if text_balloon:
-                show_img = cv2.putText(show_img, text_balloon, (int(self.x_balloon), int(self.y_balloon)), cv2.FONT_HERSHEY_DUPLEX, 2, (250,250,250), 2, cv2.LINE_AA)
-        if detection_sign and self.x_drone!=0 and self.y_drone!=0:
+                show_img = cv2.putText(show_img, text_balloon, (int(self.x_balloon), int(self.y_balloon)),
+                                       cv2.FONT_HERSHEY_DUPLEX, 2, (250, 250, 250), 2, cv2.LINE_AA)
+        if detection_sign and self.x_drone != 0 and self.y_drone != 0:
             if text_drone:
-                show_img = cv2.putText(show_img, text_drone, (int(self.x_drone), int(self.y_drone)), cv2.FONT_HERSHEY_DUPLEX, 2, (250,250,250), 2, cv2.LINE_AA)
-            show_img = cv2.circle(show_img, (int(self.x_drone), int(self.y_drone)), 15, (0,0,0), 3)
+                show_img = cv2.putText(show_img, text_drone, (int(self.x_drone), int(self.y_drone)),
+                                       cv2.FONT_HERSHEY_DUPLEX, 2, (250, 250, 250), 2, cv2.LINE_AA)
+            show_img = cv2.circle(show_img, (int(self.x_drone), int(self.y_drone)), 15, (0, 0, 0), 3)
 
         cv2.imshow(window_name, show_img)
 
 
 class ColorBound:
-
     NO_LOWER_BOUNDS = (0, 0, 0)
-    NO_UPPER_BOUNDS = (255,255, 255)
+    NO_UPPER_BOUNDS = (255, 255, 255)
 
     def __init__(self):
         self.lower = ColorBound.NO_LOWER_BOUNDS
@@ -104,36 +105,38 @@ class ColorBound:
 class ColorBounds:
 
     def __init__(self):
-            self.ball_web = ColorBound()
-            self.ball_phone = ColorBound()
-            self.drone_web = ColorBound()
-            self.drone_phone = ColorBound()
+        self.ball_left = ColorBound()
+        self.ball_right = ColorBound()
+        self.drone_left = ColorBound()
+        self.drone_right = ColorBound()
 
-    def change_ball_web(self, lower, upper):
-        self.ball_web.change(lower, upper)
+    def change_ball_left(self, lower, upper):
+        self.ball_left.change(lower, upper)
 
-    def change_ball_phone(self, lower, upper):
-        self.ball_phone.change(lower, upper)
+    def change_ball_right(self, lower, upper):
+        self.ball_right.change(lower, upper)
 
-    def change_drone_web(self, lower, upper):
-        self.drone_web.change(lower, upper)
+    def change_drone_left(self, lower, upper):
+        self.drone_left.change(lower, upper)
 
-    def change_drone_phone(self, lower, upper):
-        self.drone_phone.change(lower, upper)
+    def change_drone_right(self, lower, upper):
+        self.drone_right.change(lower, upper)
 
 
 class Image3D:
 
-    def __init__(self, image_web, image_phone, phys_x=0, phys_y=0):
-        self.frame_web = Frame(image_web)
-        self.frame_phone = Frame(image_phone)
+    def __init__(self, image_left, image_right, phys_x=0, phys_y=0):
+        self.frame_left = Frame(image_left)
+        self.frame_right = Frame(image_right)
         self.phys_x = phys_x
         self.phys_y = phys_y
 
-    def calculate_distance(self, web_fov, phone_fov, d):
-        angle_web = np.pi/2 - web_fov/2 + web_fov * (1 - self.frame_web.x_balloon / self.frame_web.image.shape[1]) # left camera
-        angle_phone = np.pi/2 - phone_fov/2 + phone_fov * self.frame_phone.x_balloon # right camera
-
-        # web camera is at (0,0)
-        self.phys_x = d * np.tan(angle_phone) / (np.tan(angle_phone) + np.tan(angle_web))
-        self.phys_y = d * np.tan(angle_phone) * np.tan(angle_web) / (np.tan(angle_phone) + np.tan(angle_web))
+    def calculate_distance(self, left, right, d):
+        p_left = (self.frame_left.image.shape[1] / 2) / np.tan(left.fov / 2)  # left camera
+        angle_left = np.pi / 2 - np.arctan2(left.flip*(self.frame_left.x_balloon - self.frame_left.image.shape[1] / 2), p_left)
+        p_right = (self.frame_right.image.shape[1] / 2) / np.tan(right.fov / 2)  # right camera
+        angle_right = np.pi / 2 - np.arctan2(right.flip*(-self.frame_right.x_balloon + self.frame_right.image.shape[1] / 2), p_right)
+        # left camera is at (0,0)
+        self.phys_x = d * np.tan(angle_right) / (np.tan(angle_right) + np.tan(angle_left))
+        self.phys_y = self.phys_x * np.tan(angle_left)
+        # print("a={}, b={}, x={}, y={}, w={}, p={}".format(np.degrees(angle_left), np.degrees(angle_right), self.phys_x, self.phys_y, self.frame_left.x_balloon, self.frame_right.x_balloon))