d0f8c74e839169a28350ba1778f7215fc741360e
orof84 <orof84@gmail.com>
2022-07-04 11:38:27 +0300

    fixes in maintaining height + return to seek when exiting hit bounds while trying to hit

--- test_2d.py
+++ test_2d.py
@@ -8,14 +8,14 @@ from camera import Camera
 from time import sleep
 
 ORI_WEB = Camera(51.3, 0, False)
-ORI_PHONE = Camera(66.9, 0, True)
-NIR_PHONE = Camera(67, 1, False)
+ORI_PHONE = Camera(66.9, 3, False)
+NIR_PHONE = Camera(67, 4, False)
 MAYA_WEB = Camera(61, 0, True)
 EFRAT_WEB = Camera(61, 0, True)
 
 COLORS_FILENAME = "color_bounds.txt"
 
-FLOOR_HEIGHT = -50
+FLOOR_HEIGHT = -70
 DRONE_DEFAULT_HEIGHT = FLOOR_HEIGHT + 50
 
 def lin_velocity_with_acc(cm_rel, real_vel):
@@ -64,14 +64,13 @@ def lin_velocity_with_two_params(cm_rel, real_velocity, direction):
         return int(velocity)
  
 
-
 def lin_velocity_z(cm_rel):
     #this function assumes the drone is looking at the cameras.
-    MAX_VEL = 20
-    A = 1.5
+    MAX_VEL = 40
+    A = 2
 
     velocity_pot = int(min(A*(abs(cm_rel)), MAX_VEL))
-    velocity = -np.sign(cm_rel) * velocity_pot
+    velocity = np.sign(cm_rel) * velocity_pot
 
     return int(velocity)
 
@@ -128,7 +127,7 @@ def track_2d(image_3d: Image3D, tello: Tello):
     if tello.send_rc_control:
         left_right = lin_velocity_with_two_params(x_cm_rel, image_3d.velocity_x_balloon, 'x')
         for_back = lin_velocity_with_two_params(y_cm_rel, image_3d.velocity_y_balloon, 'y')
-        # up_down = lin_velocity_z(z_cm_rel)
+        up_down = lin_velocity_z(z_cm_rel)
         tello.send_rc_control(left_right, for_back, up_down, 0)
 
 
@@ -155,7 +154,7 @@ def hit_ball(image: Image3D, tello: Tello):
 def hit_ball_rc(image_3d: Image3D, tello: Tello, loop_status: Status):
     UPPER_LIMIT = 170
     LOWER_LIMIT = 0
-    XY_LIMIT = 20
+    XY_LIMIT = 10
     Z_LIMIT = 15
 
     x_rel = int(image_3d.phys_x_balloon - image_3d.phys_x_drone)
@@ -184,6 +183,9 @@ def hit_ball_rc(image_3d: Image3D, tello: Tello, loop_status: Status):
         while not tello.send_rc_control:
             continue
         tello.send_rc_control(left_right, for_back, up_down, 0)
+
+    else:
+        track_2d(image_3d, tello)
 
 
 
@@ -348,6 +350,6 @@ if __name__ == "__main__":
     left = ORI_PHONE
     right = NIR_PHONE
 
-    distance = 66
+    distance = 57
     while continue_test:
         continue_test, colors = capture_video(tello, distance, left, right, colors, method='parallel')
