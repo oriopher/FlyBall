76d4369c49f9fd8de19bd7b8901479f5abf6aedb
orof84 <orof84@gmail.com>
2022-08-07 09:09:19 +0300

    doc frame

--- frame.py
+++ frame.py
@@ -6,6 +6,10 @@ class Frame:
     SEARCH_RANGE_SCALE_B = 140
 
     def __init__(self, image):
+        """
+        Initializes a frame captured from the camera.
+        :param image: the image captured.
+        """
         self.image = image
         self.threshold_size = image.shape[1] // 120
         self.capture_time = datetime.datetime.now()
@@ -13,6 +17,11 @@ class Frame:
 
     @staticmethod
     def search_range_scale(distance):
+        """
+        Calculates the scale of the search range in the frame, depending on the distance of an object from the camera.
+        :param distance: the distance of the object that will be searched for.
+        :return: the range of the search in each direction.
+        """
         if distance < 50 or distance > 500:
             return 0
         search = Frame.SEARCH_RANGE_SCALE_A * distance + Frame.SEARCH_RANGE_SCALE_B
