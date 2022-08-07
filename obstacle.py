7d8da7cb6251ce77a02996c4dba351c776bbfee5
orof84 <orof84@gmail.com>
2022-08-07 14:51:14 +0300

    fix _get_exit_dest

--- obstacle.py
+++ obstacle.py
@@ -6,12 +6,6 @@ from common import calc_linear_eq
 from quadrangle import Quadrangle
 import numpy as np
 
-MARGINS = 25
-EXIT_DIST = 10
-MARGINS_END = 25
-MARGINS_START = 5
-MARGINS_SIDES = 10
-
 """
 ###############################
 #### a-------------------d ####
@@ -25,6 +19,12 @@ MARGINS_SIDES = 10
 
 
 class Obstacle:
+    EXIT_MARGIN = 100
+    MARGINS = 25
+    MARGINS_END = 25
+    MARGINS_START = 5
+    MARGINS_SIDES = 10
+
     def __init__(self, drone, left_cam):
         self.start = (drone.x, drone.y)
         self.end = (drone.dest_coords[0], drone.dest_coords[1])
@@ -40,8 +40,8 @@ class Obstacle:
 
         # rectangle parallel to y axis
         if self.end[0] == self.start[0]:
-            x_middle_low = x_middle- MARGINS_SIDES
-            x_middle_upper = x_middle + MARGINS_SIDES
+            x_middle_low = x_middle - self.MARGINS_SIDES
+            x_middle_upper = x_middle + self.MARGINS_SIDES
             y_middle_low = y_middle
             y_middle_upper = y_middle
 
@@ -49,23 +49,23 @@ class Obstacle:
         elif self.end[1] == self.start[1]:
             x_middle_low = x_middle
             x_middle_upper = x_middle
-            y_middle_low = y_middle - MARGINS_SIDES
-            y_middle_upper = y_middle + MARGINS_SIDES
+            y_middle_low = y_middle - self.MARGINS_SIDES
+            y_middle_upper = y_middle + self.MARGINS_SIDES
 
         else:
             m_track = (self.start[1] - self.end[1]) / (self.start[0] - self.end[0])
             b_track = y_middle - m_track * x_middle
 
-            x_middle_low, y_middle_low = self._calc_coor_below(m_track, b_track, x_middle, y_middle, MARGINS_SIDES)
-            x_middle_upper, y_middle_upper = self._calc_coor_above(m_track, b_track, x_middle, y_middle, MARGINS_SIDES)
+            x_middle_low, y_middle_low = self._calc_coor_below(m_track, b_track, x_middle, y_middle, self.MARGINS_SIDES)
+            x_middle_upper, y_middle_upper = self._calc_coor_above(m_track, b_track, x_middle, y_middle, self.MARGINS_SIDES)
 
         return x_middle_low, y_middle_low, x_middle_upper, y_middle_upper
 
     # finds the vertexes of the rectangle
     def calc_corners(self):
         x_middle_low, y_middle_low, x_middle_upper, y_middle_upper = self._calc_middles()
-        length_end = 0.5 * np.sqrt((self.start[0] - self.end[0]) ** 2 + (self.start[1] - self.end[1]) ** 2) + MARGINS_END
-        length_start = 0.5 * np.sqrt((self.start[0] - self.end[0]) ** 2 + (self.start[1] - self.end[1]) ** 2) + MARGINS_START
+        length_end = 0.5 * np.sqrt((self.start[0] - self.end[0]) ** 2 + (self.start[1] - self.end[1]) ** 2) + self.MARGINS_END
+        length_start = 0.5 * np.sqrt((self.start[0] - self.end[0]) ** 2 + (self.start[1] - self.end[1]) ** 2) + self.MARGINS_START
                 
         # Horizontal rectangle
         if x_middle_upper == x_middle_low:
@@ -176,7 +176,7 @@ class Obstacle:
     def draw_obstacle(self, show_img, recognizable_object, color_in=(240, 0, 0), color_out=(0, 240, 0)):
         color = color_out
 
-        if self.inside_obstacle(self, recognizable_object):
+        if self.inside_obstacle(recognizable_object):
             color = color_in
         show_img = self.quad.draw_quadrangle(show_img, color)
 
@@ -185,7 +185,7 @@ class Obstacle:
     def bypass_obstacle_coordinates(self, source, target):
 
         if self.coord_in_obstacle(source[0], source[1]):
-            return self._get_exit_dest(source[0], source[1], 100)
+            return self._get_exit_dest(source[0], source[1], self.EXIT_MARGIN)
 
         if self.coord_in_obstacle(target[0], target[1]):
             new_target = self._get_exit_dest(target[0], target[1])
@@ -195,8 +195,8 @@ class Obstacle:
         target_vertices = np.vstack([self.coordinates, target])
         target_vertices_distance = np.insert(self._get_reachable_distances(source, target_vertices), 0, 0)
         reachable_to_target_distances = self._get_reachable_distances(target, self.coordinates)
-        corners = np.hstack([np.zeros((4, 1)), obstacle_distances, np.reshape(reachable_to_target_distances, (-1, 1))])
-        reachable_distances = np.vstack([target_vertices_distance, corners, np.zeros((1, 6))])
+        corners_distances = np.hstack([np.zeros((4, 1)), obstacle_distances, np.reshape(reachable_to_target_distances, (-1, 1))])
+        reachable_distances = np.vstack([target_vertices_distance, corners_distances, np.zeros((1, 6))])
         path = self._get_shortest_path(reachable_distances)
         if not path:
             return source
@@ -221,9 +221,9 @@ class Obstacle:
         distances = np.array([])
         # calculates squared distance to every point of te rectangle
         for coordinate in self.coordinates:
-            dist_start = (self.start[0] - coordinate[0]) ** 2  + (self.start[1] - coordinate[1]) ** 2 
-            dist_end = (self.end[0] - coordinate[0]) ** 2  + (self.end[1] - coordinate[1]) ** 2 
-            np.append(distances, dist_end - dist_start)
+            dist_start = (self.start[0] - coordinate[0]) ** 2 + (self.start[1] - coordinate[1]) ** 2
+            dist_end = (self.end[0] - coordinate[0]) ** 2 + (self.end[1] - coordinate[1]) ** 2
+            distances = np.append(distances, dist_end - dist_start)
 
         # finds 2 nearest points to end point
         first_point = self.coordinates[np.argmin(distances)]
@@ -235,15 +235,14 @@ class Obstacle:
 
         return x_dest, y_dest
 
-    # returns the nearest exit point from obsatacle
+    # returns the nearest exit point from obstacle
     def _get_exit_dest(self, x, y, exit_dist=MARGINS):
-        corners = [0, 1, 3, 2]
         curves = np.zeros((4, 2))
 
         if self.coord_in_obstacle(x, y):
             # finds rectangle curves
-            for i, cor in enumerate(corners):
-                cor_next = corners[(i + 1) % len(corners)]
+            for i, cor in enumerate(self.quad.CORNERS):
+                cor_next = self.quad.CORNERS[(i + 1) % len(self.quad.CORNERS)]
                 curves[i] = calc_linear_eq(self.coordinates[cor], self.coordinates[cor_next])
 
             distances = self._calc_distances_to_curves(curves, x, y)
@@ -252,21 +251,49 @@ class Obstacle:
             idx = np.argmin(distances)
             return self._calc_coor(curves[idx][0], curves[idx][1], x, y, exit_dist)
 
-        return 0,0    
+        return 0, 0
 
     # calculates coordinates in EXIT_DISTANCE from given curve 
     def _calc_coor(self, m, b, x, y, distance):
-        sgn = np.sign(y - m *x - b)
+        if np.isnan(m):
+            s = 0 if b > x else 1
+            return b + distance * (-1)**s, y
+        elif m == 0:
+            s = 0 if b > y else 1
+            return x, b + distance * (-1)**s
+
+        sgn = np.sign(y - m * x - b)
 
         # x,y is below the closest curve
         if sgn < 0:
-            return self._calc_coor_above(self, m, b, x, y, distance)
+            return self._calc_coor_above(m, b, x, y, distance)
 
         # x,y is above the closest curve
-        return self._calc_coor_below(self, m, b, x, y, distance) 
+        return self._calc_coor_below(m, b, x, y, distance)
+
+    def _calc_distances_to_curves(self, curves, x, y):
+        distances = np.array([])
+        for i, curve in enumerate(curves):
+            if curve[0] is None or np.isnan(curve[0]):
+                distances = np.append(distances, abs(x - self.coordinates[i][0]))
+            else:
+                distances = np.append(distances, abs(curve[0] * x - y + curve[1]) / np.sqrt(curve[0] ** 2 + 1))
+        return distances
+
+    def _continue_line_to_distance(self, x1, y1, x2, y2, r):
+        if x1 == x2:
+            s = 0 if y2 > y1 else 1
+            return x1, y1 + r * (-1)**s
+        a = (y1 - y2) / (x1 - x2)
+        b = y1 - a*x1
+        s = 0 if x2 > x2 else 1
+        x = self._solve_quadratic(a**2 + 1, 2*(a*b - x1 - a*y1), x1**2+b**2-2*b*y1-r**2, s)
+        y = a*x + b
+        return x, y
 
     # returns the coor above given line in a given distance
-    def _calc_coor_above(self, m, b, x, y, distance):
+    @staticmethod
+    def _calc_coor_above(m, b, x, y, distance):
         c = y - (-1/m) * x
         x_coor = (c - b - distance * np.sqrt(m**2 + 1)) / (m + 1/m)
         y_coor = (-1/m) * x_coor + c
@@ -274,22 +301,13 @@ class Obstacle:
         return x_coor, y_coor
 
     # return the coor below the given line in a given distance
-    def _calc_coor_below(self, m, b, x, y, distance):
+    @staticmethod
+    def _calc_coor_below(m, b, x, y, distance):
         c = y - (-1/m) * x
         x_coor = (c - b + distance * np.sqrt(m**2 + 1)) / (m + 1/m)
-        y_coor = (-1/m) * x_coor + c 
+        y_coor = (-1/m) * x_coor + c
 
         return x_coor, y_coor
-        
-    def _calc_distances_to_curves(self, curves, x, y):
-        distances = np.array([])
-        for i, curve in enumerate(curves):
-            if curve == [0, 0]:
-                np.append(distances, abs(x - self.coordinates[i][0]))
-            else:
-                np.append(distances, abs(curve[0] * x - y + curve[1]) / np.sqrt(curve[0] ** 2 + 1))
-
-        return distances
 
     @staticmethod
     def _get_shortest_path(reachable_distances):
@@ -305,17 +323,6 @@ class Obstacle:
             if 0 > vertex or vertex > target:
                 return []
         return path
-
-    def _continue_line_to_distance(self, x1, y1, x2, y2, r):
-        if x1 == x2:
-            s = 0 if y2 > y1 else 1
-            return x1, y1 + r * (-1)**s
-        a = (y1 - y2) / (x1 - x2)
-        b = y1 - a*x1
-        s = 0 if x2 > x2 else 1
-        x = self._solve_quadratic(a**2 + 1, 2*(a*b - x1 - a*y1), x1**2+b**2-2*b*y1-r**2, s)
-        y = a*x + b
-        return x, y
 
     @staticmethod
     def _solve_quadratic(a, b, c, s):
